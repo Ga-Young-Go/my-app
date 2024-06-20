f9270bcbe1ed0369021fa7a82ce056c4ecd022b3
gayoung <gy030316@gmail.com>
2024-06-20 12:48:34 +0900

    찐찐막

--- app/page.tsx
+++ app/page.tsx
@@ -30,7 +30,7 @@ export default function Home() {
             <div className="flex flex-col items-center space-y-4 text-center">
               <div className="space-y-2">
                 <h1 className="text-3xl font-bold tracking-tighter sm:text-4xl md:text-5xl lg:text-6xl/none">
-                  고가영(KoGaYoung)
+                  고가영(Ko Ga Young)
                 </h1>
                 <p className="mx-auto max-w-[700px] text-gray-500 md:text-xl dark:text-gray-400">
                  2D픽셀아트&배경 일러스트
